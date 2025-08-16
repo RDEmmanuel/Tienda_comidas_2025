545b0e71566a33721fa7b63b791262c879b973dc
Emmanuel Ruiz Diaz <rdemmanuel19@gmail.com>
2025-08-15 22:07:30 -0300

    create tables categorias productos

---
+++ database/migrations/2025_08_16_010509_create_productos_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->id();
+            
+            // Relación con categorias
+            $table->foreignId('categoria_id')->constrained('categorias')->onDelete('cascade');
+
+            $table->boolean('estado')->default(true); // true = activo, false = inactivo
+            $table->string('nombre');
+            $table->text('descripcion')->nullable();
+            $table->decimal('precio_costo', 10, 2)->nullable();
+            $table->decimal('precio_venta', 10, 2);
+            $table->integer('stock')->default(0);
+            $table->string('imagen')->nullable(); // para almacenar el nombre de la imagen o URL
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('productos');
+    }
+};
