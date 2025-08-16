26c4bc0e66bf09fa480a53ce2fafa16c2e01471c
Emmanuel Ruiz Diaz <rdemmanuel19@gmail.com>
2025-08-16 18:27:13 -0300

    guest navigation blade

---
+++ resources/views/layouts/guest_navigation.blade.php
@@ -0,0 +1,19 @@
+<nav x-data="{ open: false }" class="bg-white dark:bg-gray-800 border-b border-gray-100 dark:border-gray-700">
+    <div class="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="flex justify-between h-16">
+
+            <!-- Logo -->
+            <div class="shrink-0 flex items-center">
+                <a href="{{ url('/') }}">
+                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                </a>
+            </div>
+
+            <!-- Right Side -->
+            <div class="flex items-center space-x-4">
+                {{-- Componente Livewire CarritoBadge --}}
+                @livewire('carrito-badge')
+            </div>
+        </div>
+    </div>
+</nav>
