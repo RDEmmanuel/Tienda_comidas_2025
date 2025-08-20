ab67b8582eb45a27303778773034d306be62ea88
Emmanuel Ruiz Diaz <rdemmanuel19@gmail.com>
2025-08-20 02:20:12 -0300

    carrito icono

--- resources/views/livewire/carrito-badge.blade.php
+++ resources/views/livewire/carrito-badge.blade.php
@@ -1,6 +1,6 @@
     <div class="relative">
     <a href="{{ route('carrito') }}" class="text-gray-700 dark:text-gray-200">
-        🛒
+        <i class="fa-solid fa-cart-shopping text-white text-2xl"></i>
         @if ($cantidad > 0)
             <span class="absolute -top-2 -right-2 bg-gray-600 text-white text-xs px-2 py-0.5 rounded-full">
                 {{ $cantidad }}
