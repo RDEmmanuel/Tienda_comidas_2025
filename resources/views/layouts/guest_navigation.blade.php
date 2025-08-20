78cf2aa759162d02deb6585931c872623d254967
Emmanuel Ruiz Diaz <rdemmanuel19@gmail.com>
2025-08-20 02:19:54 -0300

    cambio de logo

--- resources/views/layouts/guest_navigation.blade.php
+++ resources/views/layouts/guest_navigation.blade.php
@@ -5,7 +5,7 @@
             <!-- Logo -->
             <div class="shrink-0 flex items-center">
                 <a href="{{ url('/') }}">
-                    <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <x-application-logo class="block h-16 w-auto fill-current text-white" />
                 </a>
             </div>
 
